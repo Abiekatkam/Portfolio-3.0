d4c5ee121047fd37bdfb63dbf9aef3db932a55a2
Abiekatkam <abhiishekkatkam30@gmail.com>
2026-07-24 22:52:29 +0530

    Added resume downloaded

--- src/components/views/resume-button.tsx
+++ src/components/views/resume-button.tsx
@@ -1,18 +1,29 @@
 import { FileText } from "lucide-react";
 import { Button } from "../../components/ui/button";
 import { cn } from "../../lib/utils";
+import Resume from "../../assets/Abhishek_Katkam_Resume_July.pdf";
 
 const ResumeButton = ({ className }: { className?: string }) => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = Resume;
+    link.download = "Abhishek_Katkam_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <Button
+      onClick={handleDownload}
       className={cn(
-        "rounded-full font-medium px-4 md:px-6 h-9 md:h-10 text-xs md:text-sm ",
+        "rounded-full font-medium px-4 md:px-6 h-9 md:h-10 text-xs md:text-sm",
         "transition-all active:scale-95 cursor-pointer",
         className
       )}
     >
-      View My Resume
-      <FileText />
+      View Resume
+      <FileText className="ml-2 h-4 w-4" />
     </Button>
   );
 };
